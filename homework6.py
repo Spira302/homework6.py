53f183082d13720b54471f93e966138ccc52ca04
Spira302 <egortretyakov2020@gmail.com>
2024-07-04 15:25:11 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,15 @@
+my_dict = {'Egor': 2005}
+print(my_dict)
+print(my_dict['Egor'])
+my_dict.update({'Lera': 2005,
+                'Sasha': 2001})
+del my_dict['Sasha']
+print(my_dict)
+my_set = {1, 2, 3, 4}
+print(set(my_set))
+my_set.add(52)
+my_set.add(66)
+my_set.discard(66)
+print(my_set)
+
+
